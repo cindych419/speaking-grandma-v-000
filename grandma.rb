75449a51513f7aaffd5805eb999e4297085d0e8d
cindych419@gmail.com <cindych419@gmail.com>
2018-04-29 17:14:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+  "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!."# Write a speak_to_grandma method.
+  "HUH?! SPEAK UP, SONNY!."# Write a speak_to_grandma method.
 end
 end
 
